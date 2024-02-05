8496c439e1cd594cb2a6cf0078121e2b80d7d81d
Mohamedkout15 <mohamed.kout@outlook.com>
2024-02-05 11:50:05 +0100

    Updated Entities , Service Implementations and Controllers

--- src/main/java/zz/sk/bankingdashboard/Repositories/ClientRepository.java
+++ src/main/java/zz/sk/bankingdashboard/Repositories/ClientRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import zz.sk.bankingdashboard.Entities.Client;
 import zz.sk.bankingdashboard.Entities.PremiereVisite;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +12,10 @@ public interface ClientRepository extends JpaRepository<Client, Long> {
     Client findClientByIdClient(String idClient);
     List<Client> findClientByPremiereVisite(PremiereVisite premiereVisite);
     List<Client> findClientsByDomaine(String Domaine);
+
+    List<Client> findClientsByAdresse_Gouvernerat(String governorat);
+
+    List<Client> findClientsByPremiereVisite_DatePrVisite(Date date);
+
+    List<Client> findClientsByDeuxiemeVisite_DateDxVisite(Date date);
 }
