38453892139c0989853a7219dd091ea5ff9ea03b
Kout Mohamed <mohamed.kout@outlook.com>
2024-02-26 01:15:37 +0100

    updated controllers

--- src/main/java/zz/sk/bankingdashboard/Configuration/CorsConfig.java
+++ src/main/java/zz/sk/bankingdashboard/Configuration/CorsConfig.java
@@ -8,15 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
+    
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                registry.addMapping("/**")// Specify the URL path for which you want to enable CORS
+                        .allowedOrigins("http://localhost:4200") // Allow requests from this origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+                        .allowedHeaders("*") // Allowed headers
+                        .allowCredentials(true); // Allow sending cookies
             }
         };
     }
